681f18fac92e56b0f8fa7a422326275817169085
Kent C. Dodds <me+github@kentcdodds.com>
2021-11-23 11:33:18 -0700

    actions!

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,3 +1,21 @@
+import type { ActionFunction } from "remix";
+import { redirect } from "remix";
+import { db } from "~/utils/db.server";
+
+export let action: ActionFunction = async ({ request }) => {
+  let form = await request.formData();
+  let name = form.get("name");
+  let content = form.get("content");
+  if (typeof name !== "string" || typeof content !== "string") {
+    throw new Error("Form submitted incorrectly");
+  }
+
+  let joke = await db.joke.create({
+    data: { name, content },
+  });
+  return redirect(`/jokes/${joke.id}`);
+};
+
 export default function NewJokeRoute() {
   return (
     <div>
