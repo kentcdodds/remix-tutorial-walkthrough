6cf077dfc0e08b53e66a69c74394cdaad0746d41
Kent C. Dodds <me+github@kentcdodds.com>
2021-11-23 09:53:04 -0700

    routes

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { LiveReload } from "remix";
+import { Outlet, LiveReload } from "remix";
 
 export default function App() {
   return (
@@ -8,7 +8,7 @@ export default function App() {
         <title>Remix: So great, it's funny!</title>
       </head>
       <body>
-        Hello world
+        <Outlet />
         {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
       </body>
     </html>
